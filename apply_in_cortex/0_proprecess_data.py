9e4377b0b0e0ff5f82a9e3664c04548bfd468c08
luluyan0606 <87313381+luluyan0606@users.noreply.github.com>
2025-08-27 16:32:01 +0800

    Update and rename 0_proprecess_data.R to 0_proprecess_data.py

---
+++ apply_in_cortex/0_proprecess_data.py
@@ -0,0 +1,31 @@
+import scvelo as scv
+import os
+import scanpy as sc
+import numpy as np
+import pandas as pd 
+import scipy.sparse as sp
+import anndata as ad
+scv.logging.print_version()
+
+print("The current work path is：", os.getcwd())
+# Load annotated binning data
+data_path = "/home/yll/velocity_methods/dataset/scSTData/2023_Biorxiv_stereoseq_mousebrain/"
+
+fname_bin60 = data_path + "mousebrain_bin60_clustered.h5ad"
+adata_bin60 = sc.read_h5ad(fname_bin60)
+
+sc.pl.embedding(adata_bin60, basis="spatial", color="scc_anno",size=5)
+
+save_path = "/home/yll/velocity_methods/01_analysis/apply_in_stereo_cortex/data/"
+
+obs = pd.DataFrame(adata_cellbin.obs)
+obs.to_csv(save_path + "cellbin_clustered_with_meta.csv")
+
+loc = pd.DataFrame(adata_cellbin.obsm['X_spatial'])
+loc.to_csv(save_path + "cellbin_clustered_with_loc.csv")
+
+count = pd.DataFrame(adata_cellbin.X.todense())
+count.to_csv(save_path + "cellbin_clustered_with_count.csv")
+
+gene_id = pd.DataFrame(adata_cellbin.var_names)
+gene_id.to_csv(save_path + "cellbin_clustered_with_gene.csv")
